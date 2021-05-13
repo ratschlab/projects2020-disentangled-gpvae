d3e2c5ef5a8e173cd8b79e069240d384de2cb62d
simonbing <bings@student.ethz.ch>
2021-05-13 11:34:49 +0200

    baseline eval script debugging

--- scripts/baseline_eval.sh
+++ scripts/baseline_eval.sh
@@ -2,4 +2,4 @@
 
 bsub -o log_%J -g /disent_baseline -R "rusage[mem=60000]" \
 python baselines/eval_baseline.py --model adagvae --base_dir gp_full_4_2 --exp_name n_1 \
---data dsprites --subset gp_full_4 --metric mig
+--data dsprites_full --subset gp_full_4 --metric mig
