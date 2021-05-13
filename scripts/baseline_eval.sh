5846c6ff61a12928bf55aa0f3af745345f219830
simonbing <bings@student.ethz.ch>
2021-05-13 12:05:24 +0200

    baseline eval script debugging

--- scripts/baseline_eval.sh
+++ scripts/baseline_eval.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-bsub -o baselines/adagvae/log_sap_n_1 -g /disent_baseline -R "rusage[mem=10000]" \
+bsub -o baselines/adagvae/log_sap_n_1 -g /disent_baseline -R "rusage[mem=30000]" \
 python baselines/eval_baseline.py --model adagvae --base_dir gp_full_4_2 --exp_name n_1 \
 --data dsprites_full --subset gp_full_4 --metric sap
