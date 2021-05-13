b74e379aa643cc277981d9aad19948ff71031440
simonbing <bings@student.ethz.ch>
2021-05-13 14:08:49 +0200

    eval script for our model

--- scripts/additional_metrics_eval.sh
+++ scripts/additional_metrics_eval.sh
@@ -5,14 +5,14 @@ bsub -o log_%J -g /gpvae_eval -R "rusage[mem=60000]" python eval_dci.py \
 --model_name /cluster/work/grlab/projects/projects2020_disentangled_gpvae/models/gp_full4/base/len_5/same/201130_n_1 \
 --data_type_dci dsprites --save_score --eval_type modularity
 
-for metric in mig modularity sap;do
-  # dSprites
-
-  # NORB
-
-  # cars3d
-
-  # shapes3d
-
-  # hirid
-done
+#for metric in mig modularity sap;do
+#  # dSprites
+#
+#  # NORB
+#
+#  # cars3d
+#
+#  # shapes3d
+#
+#  # hirid
+#done
