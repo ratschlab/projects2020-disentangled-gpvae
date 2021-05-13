659f095514fe9cca5e06e9b5f4bd86b385782b0e
simonbing <bings@student.ethz.ch>
2021-05-13 12:44:02 +0200

    baseline eval script for all datasets and models and metrics

--- scripts/baseline_eval.sh
+++ scripts/baseline_eval.sh
@@ -6,10 +6,10 @@
 
 for n in {1..10}; do
   for model in adagvae annealedvae betavae betatcvae factorvae dipvae_i dipvae_ii; do
-    for metric in mig, modularity, sap; do
+    for metric in mig modularity sap; do
       for i in dsprites_full,gp_full_4,gp_full_4_2 smallnorb,norb_full1,norb_full1_1 cars3d,cars_part1,cars_part1_2 shapes3d,shapes_part2,shapes_part2_1; do
         IFS=',' read data subset base_dir <<< "${i}"
-        bsub -o baselines/"$model"/log_"$metric"_n_"$n" -g /gpvae_eval -R "rusage[mem=30000]" \
+        bsub -o baselines/"$model"/log_"$data"_"$metric"_n_"$n" -g /gpvae_eval -R "rusage[mem=30000]" \
         python baselines/eval_baseline.py --model "$model" --base_dir "$base_dir" --exp_name n_"$n" \
         --data "$data" --subset "$subset" --metric "$metric"
       done
