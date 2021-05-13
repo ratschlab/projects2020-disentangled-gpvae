7328de8248a2926f2d13e255863db3602128959b
simonbing <bings@student.ethz.ch>
2021-05-13 14:23:10 +0200

    eval script for our model

--- scripts/additional_metrics_eval.sh
+++ scripts/additional_metrics_eval.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-bsub -o /cluster/work/grlab/projects/projects2020_disentangled_gpvae/dgpvae/eval_logs/log_%J -g /gpvae_eval -R "rusage[mem=20000]" python eval_dci.py \
+bsub -o /cluster/work/grlab/projects/projects2020_disentangled_gpvae/models/gp_full4/log_%J -g /gpvae_eval -R "rusage[mem=20000]" python eval_dci.py \
 --dci_seed 0 --c_path /cluster/work/grlab/projects/projects2020_disentangled_gpvae/data/dsprites/factors_dsprites_gp_full_range4.npz \
 --model_name /cluster/work/grlab/projects/projects2020_disentangled_gpvae/models/gp_full4/base/len_5/same/201130_n_1 \
 --data_type_dci dsprites --save_score --eval_type sap
